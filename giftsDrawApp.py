a2f6e5fc835f5b9ff0d7be244e7794a47ce88460
tbartek9 <tbartek9@gmail.com>
2023-11-25 15:39:00 +0100

    app is done, changes in giftsDrawApp.py

--- giftsDrawApp.py
+++ giftsDrawApp.py
@@ -6,10 +6,9 @@ class GiftsDrawApp:
     def __init__(self):
         self.people = self.get_people_from_user()
         self.occupied = []
-        self.results = {}
 
         for person in self.people:
-            self.results[person] = self.draw_gift_recipient(person)
+            person.result = self.draw_gift_recipient(person)
         self.save_results()
 
     def get_people_from_user(self):
@@ -20,18 +19,17 @@ class GiftsDrawApp:
 
     def get_exclusions_from_user(self, people):
         for person in people:
-            exclusion_input = input(f"Wprowadz osoby zakaznane dla {person}, odzielone przecinkiem").split(',')
+            exclusion_input = input(f"Wprowadz osoby zakaznane dla {person}, oddzielone przecinkiem").split(',')
             person.exclude_list = [excludedPerson.strip() for excludedPerson in exclusion_input]
 
     def draw_gift_recipient(self, person):
-        #available_choices do poprawy, wywala out of range
-        available_choices = [recipient for recipient in self.people if recipient != person and recipient.name not in person.exclude_list and person not in self.occupied]
+        available_choices = [participant.name for participant in self.people if participant != person and participant.name not in self.occupied and participant.name not in participant.exclude_list]
         recipient = random.choice(available_choices)
         self.occupied.append(recipient)
         return recipient
 
     def save_results(self):
-        for person, recipient in self.results.items():
-            file_path = f"data/{person}_losowanie.txt"
+        for person in self.people:
+            file_path = f"data/{person.name}_losowanie.txt"
             with open(file_path, "w") as file:
-                file.write("wylosowalas/es:"+str(recipient))
+                file.write("wylosowalas/es:"+str(person.result))
--- person.py
+++ person.py
@@ -2,6 +2,7 @@ class Person:
     def __init__(self, name):
         self.name = name
         self.exclude_list = []
+        self.result = ''
 
     def add_exclusion(self, excluded_person):
         self.exclude_list.add(excluded_person)
